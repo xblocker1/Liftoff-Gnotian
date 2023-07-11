024d155cef4c515a7689c97fd3c7ebf5b21a2bb8
Xavier <blockerxavier@gmail.com>
2023-07-10 20:15:53 -0500

    New project

---
+++ src/main/java/com/LiftoffApp/Gnotian/GnotianApplication.java
@@ -0,0 +1,13 @@
+package com.LiftoffApp.Gnotian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnotianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnotianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LiftoffApp/Gnotian/GnotianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiftoffApp.Gnotian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnotianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

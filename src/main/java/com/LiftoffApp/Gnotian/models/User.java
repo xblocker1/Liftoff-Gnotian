ee85dca9520a6ae16497bf73b49f21fcce272fce
Sarah Brown <quesarahsarah90@gmail.com>
2023-07-11 18:34:40 -0500

    1) Commented out the dependency that redirects to a login page for now. 2) Added annotation in the main application file that allows running without connecting to a DB. 3) Filled out User Class.

--- src/main/java/com/LiftoffApp/Gnotian/models/User.java
+++ src/main/java/com/LiftoffApp/Gnotian/models/User.java
@@ -2,9 +2,32 @@ package com.LiftoffApp.Gnotian.models;
 
 import jakarta.persistence.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class User extends AbstractEntity{
+    private String username;
+    private String password;
+    //private List<Artist> artistTopFive = new ArrayList<>();
+    private List<Review> userReviews = new ArrayList<>();
 
     public User(){
     }
+
+    public User(String username, String password){
+        super();
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public List<Review> getUserReviews() { return userReviews; }
 }

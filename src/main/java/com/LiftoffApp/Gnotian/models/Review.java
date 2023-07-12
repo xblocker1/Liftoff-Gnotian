4d70a10a78463a60a43204a0f7f3e8440549b531
cvnvvn <canaan.merrick@gmail.com>
2023-07-12 14:12:57 -0500

    added review model

--- src/main/java/com/LiftoffApp/Gnotian/models/Review.java
+++ src/main/java/com/LiftoffApp/Gnotian/models/Review.java
@@ -4,8 +4,62 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Review extends AbstractEntity{
+    private int reviewID;
+    private String reviewText;
+    private String artistName;
+    private int rating;
+    private int upVotes;
+    private int downVotes;
 
     public Review(){
 
+    }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getUpVotes() {
+        return upVotes;
+    }
+
+    public void setUpVotes(int upVotes) {
+        this.upVotes = upVotes;
+    }
+
+    public int getDownVotes() {
+        return downVotes;
+    }
+
+    public void setDownVotes(int downVotes) {
+        this.downVotes = downVotes;
     }
 }

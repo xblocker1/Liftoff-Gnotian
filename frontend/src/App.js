7ff9da1dc5c146b3d9958e8801b56cccce87e061
chrishahn <chrishahn97@outlookcom>
2023-07-12 12:08:15 -0500

    working on nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
+import Home from './commponets/Home';
+import Profile from './commponets/Profile';
+
+const Navigation = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <ul className="nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/"> Home </Link>
+        </li>
+       <li className="nav-item">
+          <Link className="nav-link" to="/Profile"> Profile </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navigation />
+      {children}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} /> Profile
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+};
 
 export default App;
+

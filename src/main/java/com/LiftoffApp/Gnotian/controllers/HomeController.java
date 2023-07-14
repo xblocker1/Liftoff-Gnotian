a57e551c5e4d4a156e3ac6bb9d6c3e3e65087232
Sarah Brown <quesarahsarah90@gmail.com>
2023-07-14 12:36:49 -0500

    Added working search function to homepage, as well as a link to the profile page

--- src/main/java/com/LiftoffApp/Gnotian/controllers/HomeController.java
+++ src/main/java/com/LiftoffApp/Gnotian/controllers/HomeController.java
@@ -2,7 +2,10 @@ package com.LiftoffApp.Gnotian.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -10,5 +13,16 @@ public class HomeController {
     @RequestMapping("")
     public String index(Model model){
         return "index";
+    }
+
+    @PostMapping("search")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+        model.addAttribute("searchTerm", searchTerm);
+        return "search";
+    }
+
+    @GetMapping("profile")
+    public String displayProfile(Model model){
+        return "profile";
     }
 }

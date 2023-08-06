ac4e3762633901b225a09b4a630778496cacbffd
Sarah Brown <quesarahsarah90@gmail.com>
2023-08-06 14:35:02 -0500

    Added a (commented out) Bean... trying to figure our Spring Security still

--- src/main/java/com/LiftoffApp/Gnotian/config/ProjectSecurityConfig.java
+++ src/main/java/com/LiftoffApp/Gnotian/config/ProjectSecurityConfig.java
@@ -5,7 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,9 +22,19 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/profile").authenticated()
-                .requestMatchers ("/","/search").permitAll ());
+                .requestMatchers("/", "/search").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+//@Bean
+//    public UserDetailsService userDetailsService (DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+//
+//    public DelegatingPasswordEncoder delegatingPasswordEncoder(){
+//        return delegatingPasswordEncoder().encode();
+//    }
+
 }

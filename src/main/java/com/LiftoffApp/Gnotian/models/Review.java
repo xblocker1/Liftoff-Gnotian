180a388e44b439ecb8b6dbac0d2179fa25ad3abc
cvnvvn <canaan.merrick@gmail.com>
2023-08-06 18:35:45 -0500

    merged with Sarah, working on entity relationships

--- src/main/java/com/LiftoffApp/Gnotian/models/Review.java
+++ src/main/java/com/LiftoffApp/Gnotian/models/Review.java
@@ -2,21 +2,22 @@ package com.LiftoffApp.Gnotian.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Review extends AbstractEntity{
-    private int reviewID;
-    private String reviewText;
-    private String artistName;
-    private int rating;
-    private int upVotes;
-    private int downVotes;
 
     private String reviewText;
-    @OneToOne
+
+    @ManyToOne
     @JoinColumn(name = "artist_id")
     private Artist artist;
 
@@ -42,53 +43,5 @@ public class Review extends AbstractEntity{
 
     public void setReviewText(String reviewText) {
         this.reviewText = reviewText;
-    }
-
-    public int getReviewID() {
-        return reviewID;
-    }
-
-    public void setReviewID(int reviewID) {
-        this.reviewID = reviewID;
-    }
-
-    public String getReviewText() {
-        return reviewText;
-    }
-
-    public void setReviewText(String reviewText) {
-        this.reviewText = reviewText;
-    }
-
-    public String getArtistName() {
-        return artistName;
-    }
-
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public int getUpVotes() {
-        return upVotes;
-    }
-
-    public void setUpVotes(int upVotes) {
-        this.upVotes = upVotes;
-    }
-
-    public int getDownVotes() {
-        return downVotes;
-    }
-
-    public void setDownVotes(int downVotes) {
-        this.downVotes = downVotes;
     }
 }

c486885baa3d57bad6878a88617adab8b9452056
Sarah Brown <quesarahsarah90@gmail.com>
2023-08-16 15:20:50 -0500

    Deleted all the irrelevant files. Added a field in Review to take in the Spotify user id of whoever is writing each review! Also have some more API controllers

--- src/main/java/com/LiftoffApp/Gnotian/models/data/ReviewRepository.java
+++ src/main/java/com/LiftoffApp/Gnotian/models/data/ReviewRepository.java
@@ -4,6 +4,12 @@ import com.LiftoffApp.Gnotian.models.Review;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends CrudRepository<Review,Integer> {
+
+    List<Review> findByArtist(String artist);
+
+    List<Review> findByUser(String user);
 }
